4f019677ad5b90580b80c6028320d248fb32501d
Савитский Иван Васильевич <isavitskiy@phonexit.ru>
2017-08-24 15:03:21 +0300

    Correct&clean code

--- js/setup.js
+++ js/setup.js
@@ -1,61 +1,45 @@
 'use strict';
 
-var userSetup = document.querySelector('.setup');
-userSetup.classList.remove('hidden');
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
-
-var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var charName;
+var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var CHARACTERS_NUMBER = 4;
 
 function getCharName() {
-  var randFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
-  var randLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
-  charName = randFirstName + ' ' + randLastName;
-  return charName;
+  var randFirstName = FIRST_NAMES[Math.floor(Math.random() * FIRST_NAMES.length)];
+  var randLastName = LAST_NAMES[Math.floor(Math.random() * LAST_NAMES.length)];
+  return randFirstName + ' ' + randLastName;
 }
 
-var randcoatColor;
 function getCoatColor() {
-  var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  randcoatColor = coatColors[Math.floor(Math.random() * coatColors.length)];
-  return randcoatColor;
+  return COAT_COLORS[Math.floor(Math.random() * COAT_COLORS.length)];
 }
 
-var randEyesColor;
 function getEyesColor() {
-  var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-  randEyesColor = eyesColors[Math.floor(Math.random() * eyesColors.length)];
-  return randEyesColor;
+  return EYES_COLORS[Math.floor(Math.random() * EYES_COLORS.length)];
 }
 
-var characters = [
-  {
+function getRandCharParams() {
+  return {
     name: getCharName(),
     coatColor: getCoatColor(),
     eyesColor: getEyesColor()
-  },
-  {
-    name: getCharName(),
-    coatColor: getCoatColor(),
-    eyesColor: getEyesColor()
-  },
-  {
-    name: getCharName(),
-    coatColor: getCoatColor(),
-    eyesColor: getEyesColor()
-  },
-  {
-    name: getCharName(),
-    coatColor: getCoatColor(),
-    eyesColor: getEyesColor()
-  },
-];
+  };
+}
 
-function renderWizard(wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+function createCharacters(charactersNumber) {
+  var wizards = [];
+
+  for (var i = 0; i < charactersNumber; i++) {
+    wizards[i] = getRandCharParams();
+  }
+
+  return wizards;
+}
+
+function renderWizard(wizard, template) {
+  var wizardElement = template.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
@@ -64,10 +48,22 @@ function renderWizard(wizard) {
   return wizardElement;
 }
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < characters.length; i++) {
-  fragment.appendChild(renderWizard(characters[i]));
-}
-similarListElement.appendChild(fragment);
+function init() {
+  var userSetup = document.querySelector('.setup');
+  userSetup.classList.remove('hidden');
 
-userSetup.querySelector('.setup-similar').classList.remove('hidden');
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+  var characters = createCharacters(CHARACTERS_NUMBER);
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < characters.length; i++) {
+		fragment.appendChild(renderWizard(characters[i], similarWizardTemplate));
+  }
+
+  similarListElement.appendChild(fragment);
+  userSetup.querySelector('.setup-similar').classList.remove('hidden');
+}
+
+init();
